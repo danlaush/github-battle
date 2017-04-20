ff19e35112f1ef77c1c61a5f46460805db58f12b
Dan Laush <dan.laush@gmail.com>
2017-04-21 08:26:21 +1000

    Set up webpack, add Popular component

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+var App = require('./components/App');
+
+// State
+
+// Lifecycle event
+
+// UI - only required component
+
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+)
